ce81b6584dbbb42a9860809722ff480ae0826e89
dpqls <dpqls0625@naver.com>
2022-07-26 12:43:15 +0900

    예외 발생

--- Java_server/src/MyStream.java
+++ Java_server/src/MyStream.java
@@ -3,11 +3,11 @@ public class MyStream {
         MyStream mystream=new MyStream();
         int result;
 		try{
-            result=100/0;
-        }catch(ArithmeticException e){
+            result=100/0; // 예외 발생
+        }catch(ArithmeticException e){ // 예외 처리
             result=-1;
             System.out.println("Error");
-        }finally{
+        }finally{ // 예외가 발생하더라도 수행
             mystream.run();
         }
 		
