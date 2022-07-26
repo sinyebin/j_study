55308635ec0b5794a3b1dc89859d562952459eb5
dpqls <dpqls0625@naver.com>
2022-07-26 12:50:08 +0900

    예외 처리

--- Java_server/src/MyStream.java
+++ Java_server/src/MyStream.java
@@ -1,12 +1,16 @@
 public class MyStream {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
         MyStream mystream=new MyStream();
         int result;
 		try{
             result=100/0; // 예외 발생
+            mystream.input("qwe"); 
+            mystream.input("test"); //예외 발생
         }catch(ArithmeticException e){ // 예외 처리
             result=-1;
-            System.out.println("Error");
+            System.out.println("by zero");
+        }catch(Exception e){
+            System.out.println("test is not valid name");
         }finally{ // 예외가 발생하더라도 수행
             mystream.run();
         }
@@ -14,5 +18,10 @@ public class MyStream {
 	}
     public void run(){
         System.out.printf("running!!");
+    }
+    public void input(String name) throws Exception{ // 예외 처리
+        if(name.equals("test")){
+            throw new Exception(); // 예외 발생
+        }
     }
 }
