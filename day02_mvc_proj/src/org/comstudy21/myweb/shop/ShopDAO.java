ea89875bcec17b5ffc53ac1f01bb94768480f961
dpqls <dpqls0625@naver.com>
2022-08-18 20:23:07 +0900

    상품 전체 리스트

--- day02_mvc_proj/src/org/comstudy21/myweb/shop/ShopDAO.java
+++ day02_mvc_proj/src/org/comstudy21/myweb/shop/ShopDAO.java
@@ -1,35 +1,56 @@
 package org.comstudy21.myweb.shop;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.comstudy21.myweb.controller.JdbcUtil;
 
 public class ShopDAO {
-	private static List<ShopDTO> shopList;
-	private static int no=1;
-	static {
-		shopList=new ArrayList<ShopDTO>();
-		shopList.add(new ShopDTO(no++,"아이스티",3000));
-	}
-	public static List<ShopDTO> selectAll(){
+	public static final String SELECT = "SELECT * FROM SHOP";
+	public static final String INSERT = "INSERT INTO SHOP(GOODS,PRICE)VALUES(?,?)";
+	public static final String UPDATE = "UPDATE SHOP SET GOODS=?, PRICE=? WHERE NO=?";
+	public static final String SELECT_ONE = "SELECT * FROM SHOP WHERE NO=?";
+	public static final String DELETE = "DELETE FROM SHOP WHERE NO=?";
+	private Connection conn;
+	private Statement stmt;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	public List<ShopDTO> selectAll() throws SQLException{
+		List<ShopDTO> shopList = new ArrayList();
+		conn= JdbcUtil.getConnection();
+		stmt=conn.createStatement();
+		rs=stmt.executeQuery(SELECT);
+		while(rs.next()) {
+			ShopDTO dto = new ShopDTO();
+			dto.setNo(rs.getInt("no"));
+			dto.setGoods(rs.getString("goods"));
+			dto.setPrice(rs.getInt("price"));
+			shopList.add(dto);
+		}
+		JdbcUtil.close(conn, stmt, rs);
 		return shopList;
 	}
+
 	public void insert(ShopDTO dto) {
-		dto.setNo(no++);
-		shopList.add(dto);
-		//System.out.println(boardList);
+
 	}
-	   
+
 	public ShopDTO selectOne(ShopDTO dto) {
 		return null;
 	}
-	   
+
 	public void update(ShopDTO dto) {
-	      
+
 	}
-	   
+
 	public void delete(ShopDTO dto) {
-	      
+
 	}
 
 }
--- day02_mvc_proj/src/org/comstudy21/myweb/shop/service/ShopListService.java
+++ day02_mvc_proj/src/org/comstudy21/myweb/shop/service/ShopListService.java
@@ -1,5 +1,6 @@
 package org.comstudy21.myweb.shop.service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,7 +11,13 @@ import org.comstudy21.myweb.shop.ShopDTO;
 
 public class ShopListService implements ShopService{
 	public String service(HttpServletRequest req, HttpServletResponse resp) {
-		List<ShopDTO> shopList= ShopDAO.selectAll();
+		List<ShopDTO> shopList=null;
+		try {
+			shopList = shopDAO.selectAll();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		req.setAttribute("shopList", shopList);
 		return "shop/list";
 	}
