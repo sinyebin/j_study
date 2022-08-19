7751a7cd1dba31168367b9cc5c2e6f7340b14f72
dpqls <dpqls0625@naver.com>
2022-08-19 17:42:41 +0900

    json

--- dya05_js_ex01/src/day05/Day05Servlet.java
+++ dya05_js_ex01/src/day05/Day05Servlet.java
@@ -1,6 +1,8 @@
 package day05;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,18 +12,37 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data.do")
 public class Day05Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
 	protected void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
 		req.setCharacterEncoding("UTF-8");
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/html;charset=UTF-8");
+		/*
+		 * html PrintWriter out = resp.getWriter(); out.println("<html>" + "<body>"
+		 * +" <h1>Hello world</h1>"+"</body>"+"</html>"); out.close();
+		 */
+		PrintWriter out = resp.getWriter();
+		int no = Integer.parseInt(req.getParameter("no"));
+		String name = req.getParameter("name");
+		String phone = req.getParameter("phone");
+		String email = req.getParameter("email");
+
+		String jsonStr = "{\"no\":" + no + ",\"name\":\"" + name + "\",\"phone\":\"" + phone + "\",\"email\":\"" + email
+				+ "\"}";
+		out.println(jsonStr);
+
+		out.close();
+
 	}
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doProcess(req,resp);
+		doProcess(req, resp);
 	}
-	 @Override
+
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		 doProcess(req,resp);
+		doProcess(req, resp);
 	}
-	 
 
 }
