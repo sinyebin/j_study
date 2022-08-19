c9318d071d238c71369198fc98a7da1b87caa22d
dpqls <dpqls0625@naver.com>
2022-08-19 11:12:49 +0900

    junit

--- day04_junit_ex01/test/resources/org/comstude21/myapp/ex02/SimpleCalculatorTest.java
+++ day04_junit_ex01/test/resources/org/comstude21/myapp/ex02/SimpleCalculatorTest.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class SimpleCalculatorTest {
 	SimpleCalculator calc;
 	@BeforeAll
@@ -29,23 +33,34 @@ class SimpleCalculatorTest {
 	}
 
 	@Test
-	void testAdd() {
-		assertEquals(3, calc.add(1, 2));
-	}
-
-	@Test
-	void testSub() {
+	@DisplayName("더하기 기능 테스트")
+	void test_Add() {
+		System.out.println("calc.add(1, 2) ==> "+calc.add(1, 2));
+		int result=calc.add(1, 2);
+		assertAll( //여러개 검사하고 싶을때 사용
+				() -> assertEquals(3, calc.add(1, 2)),
+				() -> assertTrue(result>0)
+		);
+		System.out.println("더하기 다음 기능");
 		
 	}
 
 	@Test
-	void testMul() {
-		
+	@DisplayName("빼기 기능 테스트")
+	void test_Sub() {
+		System.out.println("빼기");
 	}
 
 	@Test
-	void testDiv() {
-		
+	@DisplayName("곱하기 기능 테스트")
+	void test_Mul() {
+		System.out.println("곱하기");
+	}
+
+	@Test
+	@DisplayName("나누기 기능 테스트")
+	void test_Div() {
+		System.out.println("나누기");
 	}
 
 }
